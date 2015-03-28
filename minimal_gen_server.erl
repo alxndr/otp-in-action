0e25867dae9c69988a2a3599cfe8a02d8cf79428
Alexander <alxndr+github@gmail.com>
2015-03-28 13:49:58 -0700

    minimal `gen_server` behavior implementation
    Listing 3.1, p 98

---
+++ minimal_gen_server.erl
@@ -0,0 +1,33 @@
+-module(minimal_gen_server).
+
+-behavior(gen_server).
+
+-export(
+  [init/1,
+  handle_call/3,
+  handle_cast/2,
+  handle_info/2,
+  terminate/2,
+  code_change/3
+  ]).
+
+-record(state, {}).
+
+init([]) ->
+  {ok, #state{}}.
+
+handle_call(_Request, _From, State) ->
+  Reply = ok,
+  {reply, Reply, State}.
+
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVersion, State, _Extra) ->
+  {ok, State}.
